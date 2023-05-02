73bed0548c5d60c39fd1823313940e7fab0b078b
Lucao Adam <lucoadam@gmail.com>
2023-05-02 15:13:43 +0545

    backend init

---
+++ index.js
@@ -0,0 +1,9 @@
+import express from 'express';
+
+
+const app = express()
+
+
+app.listen(8000, () => {
+    console.log("Server running on :8000")
+})
--- src/pages/about.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Layout from "../layouts";
-
-const About = () => {
-  return <Layout>About page</Layout>;
-};
-
-export default About;
